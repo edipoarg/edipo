6ce28a7eeb68dc3f15193237b029d6d8e83c24d2
anon author <noemail@nothing.null>
2024-06-18 14:35:30 -0300

    base config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
-/// <reference types="vitest" />
-
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "./",
-  test: {
-    globals: true,
-  },
-});
+})
