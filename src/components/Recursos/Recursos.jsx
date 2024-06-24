0c86e3789c09523bd6733d9b8b01119d3cc8b08b
Lidiador <alex.ai.conejo@gmail.com>
2024-06-24 20:56:43 -0300

    recursos base css

--- src/components/Recursos/Recursos.jsx
+++ src/components/Recursos/Recursos.jsx
@@ -1,21 +1,22 @@
 import { Link } from "react-router-dom";
-
+import styles from "./Recursos.module.css"
 const Recursos = () =>{
 
 return(
     <>
-    <section >
+    <section className={styles.header}>
     <h2>recursos</h2>
     </section>
-    <section>
+    <section className={styles.recursos}>
     <Link>
-    <div>  <h3>seguridad</h3></div>
+    <div className={styles.seguridad}>  <h3 className={styles.item}>seguridad</h3></div>
+    
     </Link>
     <Link>
-       <div><h3>busqueda</h3></div> 
+       <div className={styles.busqueda}><h3 className={styles.item}>busqueda</h3></div> 
         </Link>
         <Link>
-        <div><h3>Guia mapa</h3></div>
+        <div className={styles.guiaMapa}><h3 className={styles.item}>Guia mapa</h3></div>
         </Link>
     </section>
 
