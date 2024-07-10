0b8109b7b34baa20fe3dd056798ed0757677c1ae
anon author <noemail@nothing.null>
2024-07-10 14:28:07 -0300

    Publicaiones.jsx y modulo css, conexion a main y navbar y textos basicos

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -9,10 +9,10 @@ const historiaDescripcion = "Surgió en 2018 como un organismo de derechos human
 
 const misionTitulo = "Visión, Misión, Vínculo con Revista Crisis";
 const misionDescripciones = [
-  "El Equipo de Investigación Política de la Revista Crisis es un colectivo que explora estrategias innovadoras para la defensa de los derechos humanos a través del desarrollo de plataformas tecnológicas de participación ciudadana bajo las premisas de democratización de información y construcción colaborativa de conocimiento.",
-  "Ante el desafío cada vez más urgente de ¿qué hacer ante las violencias contemporáneas?, EdIPo genera herramientas, información y análisis que enriquezcan el debate público y fortalezcan las respuestas comunitarias.",
-  "Ya sea tirando del hilo hacia análisis en profundidad o a través de radiografías más extensas como mapas de poderes, la Investigación Política asume la información como territorio de disputa para dar nombre a una experimentación abierta e interdisciplinaria.",
-  "Desarrolla capacidades colectivas y conectivas de investigación, de nombrar, atestiguar y denunciar violencias, de defenderse mediante procesos de construcción social de evidencia, monitoreo colaborativo y construcción de registros independientes para fomentar miradas críticas, narrativas alternativas e intervenciones estratégicas."
+    "El Equipo de Investigación Política de la Revista Crisis es un colectivo que explora estrategias innovadoras para la defensa de los derechos humanos a través del desarrollo de plataformas tecnológicas de participación ciudadana bajo las premisas de democratización de información y construcción colaborativa de conocimiento.",
+    "Ante el desafío cada vez más urgente de ¿qué hacer ante las violencias contemporáneas?, EdIPo genera herramientas, información y análisis que enriquezcan el debate público y fortalezcan las respuestas comunitarias.",
+    "Ya sea tirando del hilo hacia análisis en profundidad o a través de radiografías más extensas como mapas de poderes, la Investigación Política asume la información como territorio de disputa para dar nombre a una experimentación abierta e interdisciplinaria.",
+    "Desarrolla capacidades colectivas y conectivas de investigación, de nombrar, atestiguar y denunciar violencias, de defenderse mediante procesos de construcción social de evidencia, monitoreo colaborativo y construcción de registros independientes para fomentar miradas críticas, narrativas alternativas e intervenciones estratégicas."
 ];
 
 const internacionalTitulo = "Colaboraciones internacionales";
@@ -22,42 +22,42 @@ const equipoTitulo = "Equipo";
 const equipoDescripcion = "Aquí puedes añadir información sobre cada participante";
 
 const Contacto = () => {
-  return (
-    <>
-      <section className={styles.header}>
-        <h2>{tituloPrincipal}</h2>
-        <h2>{subtituloPrincipal}</h2>
-      </section>
+    return (
+        <>
+            <section className={styles.header}>
+                <h2>{tituloPrincipal}</h2>
+                <h2>{subtituloPrincipal}</h2>
+            </section>
 
-      <section className={styles.contacto}>
-        <div className={styles.tematica}>
-          <Historia />
-          <h3>{historiaTitulo}</h3>
-          <h5>{historiaDescripcion}</h5>
-        </div>
+            <section className={styles.contacto}>
+                <div className={styles.tematica}>
+                    <Historia />
+                    <h3>{historiaTitulo}</h3>
+                    <h5>{historiaDescripcion}</h5>
+                </div>
 
-        <div className={styles.tematica}>
-          <Mision />
-          <h3>{misionTitulo}</h3>
-          {misionDescripciones.map((desc, index) => (
-            <h5 key={index}>{desc}</h5>
-          ))}
-        </div>
+                <div className={styles.tematica}>
+                    <Mision />
+                    <h3>{misionTitulo}</h3>
+                    {misionDescripciones.map((desc, index) => (
+                        <h5 key={index}>{desc}</h5>
+                    ))}
+                </div>
 
-        <div className={styles.tematica}>
-          <Internacional />
-          <h3>{internacionalTitulo}</h3>
-          <h5>{internacionalDescripcion}</h5>
-        </div>
+                <div className={styles.tematica}>
+                    <Internacional />
+                    <h3>{internacionalTitulo}</h3>
+                    <h5>{internacionalDescripcion}</h5>
+                </div>
 
-        <div className={styles.tematica}>
-          <Equipo />
-          <h3>{equipoTitulo}</h3>
-          <h5>{equipoDescripcion}</h5>
-        </div>
-      </section>
-    </>
-  );
+                <div className={styles.tematica}>
+                    <Equipo />
+                    <h3>{equipoTitulo}</h3>
+                    <h5>{equipoDescripcion}</h5>
+                </div>
+            </section>
+        </>
+    );
 };
 
 export default Contacto;
