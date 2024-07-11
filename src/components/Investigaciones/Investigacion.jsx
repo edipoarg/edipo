cc45775ee65fbb62a0f74888c45a1963109003c6
Lidiador <alex.ai.conejo@gmail.com>
2024-07-11 11:54:42 -0300

    Navbar fixed

--- src/components/Investigaciones/Investigacion.jsx
+++ src/components/Investigaciones/Investigacion.jsx
@@ -10,17 +10,30 @@ const Investigacion = () => {
     return <p>La investigación no fue encontrada.</p>;
   }
 
-  const { nombre, aclaracion, textoLargo, enlace } = investigacion;
+  const { titulo, bajada, año, descripcion, enlace, ig, tw, fb, mail} = investigacion;
 
   return (
     <div className="investigation-container">
 
-      <h2>{nombre}</h2>
+      <h2>{titulo}</h2>
 
-      <h3>{aclaracion}</h3>
-      <p>{textoLargo}</p>
+      <h3>{bajada}</h3>
+      <h3>{año}</h3>
+      <p>{descripcion}</p>
       <a href={enlace} target="_blank" rel="noopener noreferrer">
         <button>Ver más</button>
+      </a>
+      <a href={ig} target="_blank" rel="noopener noreferrer">
+        <button>ig</button>
+      </a>
+      <a href={tw} target="_blank" rel="noopener noreferrer">
+        <button>tw</button>
+      </a>
+      <a href={fb} target="_blank" rel="noopener noreferrer">
+        <button>fb</button>
+      </a>
+      <a href={mail} target="_blank" rel="noopener noreferrer">
+        <button>mail</button>
       </a>
     </div>
   );
--- src/components/Investigaciones/InvestigacionesList.jsx
+++ src/components/Investigaciones/InvestigacionesList.jsx
@@ -1,28 +1,27 @@
 // En un archivo llamado investigationsData.js
 export const investigationsData = [
     {
-      nombre: "Mapa de la policía",
-
-      aclaracion: "El Mapa de la Policía de la Ciudad de Buenos Aires es una herramienta de cuidado ciudadano que surge para contrarrestar los abusos policiales y la violencia institucional. Fue lanzado en el año 2022 -con la presentación del sitio web mapadelapolicia.com-  y es un proyecto que se encuentra vigente y en curso.",
-
-      textoLargo: "Surge en un contexto de creciente deterioro social, en una ciudad capital donde los organismos de control no cumplen la función de supervisar las funciones de las fuerzas de seguridad, contando con un presupuesto significativo destinado a las mismas. La iniciativa parte de una intuición clave: democratizar la información es hoy una herramienta fundamental  para enfrentar la crueldad de los poderes. Se compone de una red de personas y organizaciones que tienen el objetivo de construir estrategias novedosas. Entre sus impulsores hay instituciones de vasta trayectoria en el campo de los Derechos Humanos. Entre ellas se destacan: el CELS (Centro de Estudios Legales y Sociales) y ACVI (Asociación de Lucha contra la Violencia Institucional).",
-
+      titulo: "Mapa de la policía",
+      bajada: "Red de cuidado ciudadano",
+      año: "2022",
+      descripcion: "El Mapa de la Policía es una herramienta de cuidado ciudadano que surge para contrarrestar la brutalidad policial. Se trata de un proyecto colaborativo impulsado por el Equipo de Investigación Política (EdiPO) junto a el Centro de Estudios Legales y Sociales (CELS), la Coordinadora contra la Represión Policial (CORREPI) y el Movimiento de Trabajadores Excluidos (MTE), entre otros colectivos y organizaciones sociales. Fue lanzado en el año 2022, en la Ciudad de Buenos Aires, con la presentación del sitio web mapadelapolicia.com y con el objetivo de desarrollar y poner a disposición las herramientas técnicas necesarias para que distintos colectivos locales afectados por este flagelo puedan replicar el proyecto en sus territorios.  En línea con esta apuesta, en mayo se lanzará el nodo La Plata (Provincia de Buenos Aires) y, antes del fin de 2024, está prevista su puesta en funcionamiento en Rosario (Santa Fe). En 2022 decíamos que el Mapa surgía en un contexto de creciente deterioro social y en una ciudad capital donde los organismos de control no cumplían la función de supervisar las funciones de su Policía de la Ciudad, contando con un presupuesto significativo destinado a las mismas. Luego de la llegada al poder del líder de ultraderecha Javier Milei, debemos profundizar la advertencia: solo en sus primeros tres meses de Gobierno, la pobreza alcanzó a tres millones más de personas, siendo los varones de ese estrato social el objetivo principal del hostigamiento policial. Mientras, en el plano securitario, ya no se trata solo de falta de controles sino que distintas reformas le otorgaron mayores facultades represivas a las fuerzas de seguridad, en colisión con las recomendaciones y tratados internacionales de Derechos Humanos en la materia, que en nuestro país tienen rango constitucional.En este marco, reafirmamos que democratizar la información es hoy una herramienta fundamental  para enfrentar la crueldad de los poderes.La herramienta digital ofrece: Información relevante sobre el accionar de la Policía de la Ciudad. Un formulario de denuncia accesible para visibilizar hechos de violencia policial. Recursos sobre qué hacer ante los abusos y la represión, y recomendaciones sobre cómo registrar este tipo de hechos. Desde su lanzamiento (11 de julio de 2022) el sitio recibió 80.432 ingresos y 236 denuncias por medio del formulario web, de las cuales 107 fueron verificadas y 96 visibilizadas en redes sociales. En Instagram, alcanza a una comunidad de 12.400 seguidores, siendo esta el principal canal de difusión de la plataforma participado de intercambios para mostrar la herramienta, y hemos recibido interés de otros países en reproducirla. En cuanto a las sesiones en el sitio web, se destacan los siguientes países, más allá de Argentina: España (766), Uruguay (451), Estados Unidos (346), Brasil (227), México (172) e Italia (148).",
       enlace: "https://www.mapadelapolicia.com",
-
       dominio: 'mapa',
 
     },
     {
-      nombre: "Ra-dar",
-      aclaracion: "Registro de ataques de derechas argentinas radicalizadas",
-      textoLargo: "RADAR es un proyecto periodístico colaborativo impulsado por el Equipo de Investigación Política (EdIPo) de la Revista Crisis con apoyo del Centro de Estudios Legales y Sociales (CELS) con el objetivo de fortalecer la democracia y la defensa de los derechos humanos. Presentamos un mapeo colaborativo de ataques protagonizados por derechas radicalizadas con el fin de contribuir al diagnóstico colectivo y la elaboración de estrategias de autocuidado.",
+      titulo: "Ra-dar",
+      bajada: "Registro de ataques de derechas argentinas radicalizadas",
+      año: "2023",
+      descripcion: "RADAR es un proyecto periodístico colaborativo impulsado por el Equipo de Investigación Política (EdIPo) de la Revista Crisis con apoyo del Centro de Estudios Legales y Sociales (CELS) con el objetivo de fortalecer la democracia y la defensa de los derechos humanos. Presentamos un mapeo colaborativo de ataques protagonizados por derechas radicalizadas con el fin de contribuir al diagnóstico colectivo y la elaboración de estrategias de autocuidado.",
       enlace: "http://ra-dar.com.ar",
       dominio: 'radar',
     },
     {
-      nombre: "A.H.O.R.A.",
-      aclaracion: "Ahrchivo historico organizado de la represion argentina",
-      textoLargo: "Información detallada sobre la composición del suelo y su importancia.",
+      titulo: "A.H.O.R.A.",
+      bajada: "Ahrchivo historico organizado de la represion argentina",
+      año: "2024",
+      descripcion: "Información detallada sobre la composición del suelo y su importancia.",
       enlace: "http://archivo.ar",
       dominio: 'ahora',
 
